0874190e396d148b2e221fd8053e5d63839e0b79
Carl Howells <chowells@others.local>
2010-06-25 12:50:49 -0700

    Add file I forgot to include in the last commit

---
+++ src/Snap/Heist.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Snap.Heist where
+
+import qualified Data.ByteString.Char8 as S
+
+import           Snap.Types
+import           Text.Templating.Heist
+
+
+render :: TemplateState Snap -> S.ByteString -> Snap ()
+render ts template = do
+    bytes <- renderTemplate ts template
+    flip (maybe missingTemplate) bytes $ \x -> do
+        modifyResponse $ setContentType "text/html; charset=utf-8"
+                       . setContentLength (fromIntegral $ S.length x)
+        writeBS x
+  where
+    missingTemplate = do
+        let msg = S.append "Unable to load template: " template
+        modifyResponse $ setContentType "text/plain; charset=utf-8"
+                       . setContentLength (fromIntegral $ S.length msg)
+                       . setResponseStatus 500 "Internal Server Error"
+        writeBS msg
