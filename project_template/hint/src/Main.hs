8937d0e9c5dd0a04f9eaf48a047dcfcebc53fddf
Carl Howells <chowells@others.local>
2010-06-30 15:45:05 -0700

    Prettify and document the hint template Main.hs

--- project_template/hint/src/Main.hs
+++ project_template/hint/src/Main.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE CPP, TemplateHaskell #-}
 module Main where
 
-import Data.Monoid (mappend, mempty)
+import Data.Monoid        (mappend, mempty)
 
-import Config (getConfig, cleanupConfig)
-import Site (site)
+import Config             (getConfig, cleanupConfig)
+import Site               (site)
 
 import Snap.Http.Server
 import Snap.Http.Server.Config
@@ -12,20 +12,39 @@ import Snap.Http.Server.Config
 #ifdef PRODUCTION
 import Snap.Loader.Static (loadSnapTH)
 #else
-import Snap.Loader.Hint (loadSnapTH)
+import Snap.Loader.Hint   (loadSnapTH)
 #endif
 
+-- This is the entry point for this web server application.  It
+-- supports easily switching between interpreting source and running
+-- statically compiled code.
 main :: IO ()
 main = do
-    (cleanup, snap) <- $(loadSnapTH 'getConfig 'cleanupConfig 'site)
-
+    -- override the some of the defaults from Snap.Http.Server.Config
     let defaultFlags = mempty { flagVerbose = True
                               , flagAccessLog = Just "log/access.log"
                               , flagErrorLog = Just "log/error.log"
                               }
 
+    -- read command line args, and merge them with the defaults above
     cmdLineFlags <- readFlagsFromCmdLineArgs
     let conf = flagsToConfig $ defaultFlags `mappend` cmdLineFlags
 
+    -- This is just about the same as calling a function:
+    --
+    -- loadSnap :: IO a
+    --          -> (a -> IO ())
+    --          -> (a -> Snap ())
+    --          -> IO (IO (), Snap ())
+    --
+    -- The important parts are that it gives you back a cleanup action
+    -- and a Snap handler.  The specific behavior of each depends on
+    -- whether the Hint loader or the Static loader is imported.  This
+    -- interface abstracts across the differences between them.
+    (cleanup, snap) <- $(loadSnapTH 'getConfig 'cleanupConfig 'site)
+
+    -- Run the server
     httpServeConfig conf snap
+
+    -- Run the cleanup action before exiting
     cleanup
