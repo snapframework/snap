460e7cc7ee82ee3eaef23ddb7b7d2a8806af0ea9
Carl Howells <chowells@others.local>
2010-06-30 09:16:31 -0700

    Document Snap.Loader.Hint

--- src/Snap/Loader/Static.hs
+++ src/Snap/Loader/Static.hs
@@ -1,13 +1,28 @@
 {-# LANGUAGE TemplateHaskell #-}
-
+-- | This module is a source-compatible replacement for
+-- "Snap.Loader.Hint".  It does not import the GHC api, directly or
+-- transitively, resulting in a greatly decreased binary size, as
+-- compared to the Hint loader.
+--
+-- This module results in the same code as using the actions in the
+-- obvious, straight-forward manner.  It is present *only* as a
+-- source-level replacement for the Hint loader, to enable quickly
+-- switching the Hint loader off with only a changed import.
 module Snap.Loader.Static where
 
-------------------------------------------------------------------------------
 import           Control.Arrow
 import           Language.Haskell.TH.Syntax
 
+
 ------------------------------------------------------------------------------
--- | XXX
+-- | This function is a shim for source compatibility with loadSnapTH
+-- in "Snap.Loader.Hint".  This function is a TH wrapper around a
+-- hypothetical function:
+--
+-- > loadSnap :: IO a -> (a -> IO ()) -> (a -> Snap ()) -> IO (IO (), Snap ())
+-- > loadSnap initialize cleanup action = do
+-- >     i <- initialize
+-- >     return (cleanup i, action i)
 loadSnapTH :: Name -> Name -> Name -> Q Exp
 loadSnapTH initialize cleanup action = do
     funE <- [| \c a -> fmap (c &&& a) |]
