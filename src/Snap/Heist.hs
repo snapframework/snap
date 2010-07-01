34d7fc488e0aaf1433de4c278166ead7323366b5
Carl Howells <chowells@others.local>
2010-07-01 16:44:50 -0700

    Add documentation for Snap.Heist

--- src/Snap/Heist.hs
+++ src/Snap/Heist.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
+-- | This module contains convenience functions for helping render
+-- Heist templates from Snap.
 module Snap.Heist where
 
+------------------------------------------------------------------------------
 import qualified Data.ByteString.Char8 as S
 
 import           Snap.Error
@@ -9,12 +12,22 @@ import           Snap.Types
 import           Text.Templating.Heist
 
 
+------------------------------------------------------------------------------
+-- | This is a convenience function.  It calls 'render' with the
+-- content type set to @text/html; charset=utf-8@.
 renderHtml :: (MonadSnap m) => TemplateState m -> S.ByteString -> m ()
 renderHtml = render "text/html; charset=utf-8"
 
 
-render :: (MonadSnap m) =>
-          S.ByteString -> TemplateState m -> S.ByteString -> m ()
+------------------------------------------------------------------------------
+-- | Renders a template with the provided content type.  If the
+-- template cannot be loaded, 'internalError' is called with an error
+-- message.
+render :: (MonadSnap m)
+       => S.ByteString -- ^ the content type to include in the response
+       -> TemplateState m -- ^ the TemplateState that contains the template
+       -> S.ByteString -- ^ the name of the template
+       -> m ()
 render contentType ts template = do
     bytes <- renderTemplate ts template
     flip (maybe missingTemplate) bytes $ \x -> do
