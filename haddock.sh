2a1830c140c6cd93c5bf9d49c5c0aa80e49b3d8a
Gregory Collins <greg@gregorycollins.net>
2010-12-11 16:46:30 +0100

    Add haddock shell script

---
+++ haddock.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -x
+
+HADDOCK_OPTS='--html-location=http://hackage.haskell.org/packages/archive/$pkg/latest/doc/html --css=extra/haddock.css'
+
+cabal haddock $HADDOCK_OPTS --hyperlink-source $@
+
+cp -r extra/fonts dist/doc/html/snap/
+cp extra/logo.gif dist/doc/html/snap/haskell_icon.gif
+cp extra/hscolour.css dist/doc/html/snap/src/
