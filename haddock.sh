144a6c3d75d08ed332388b3ce11f2d292199ffeb
Gregory Collins <greg@gregorycollins.net>
2011-02-05 15:17:34 -0500

    Fix haddock.sh

--- haddock.sh
+++ haddock.sh
@@ -6,6 +6,5 @@ HADDOCK_OPTS='--html-location=http://hackage.haskell.org/packages/archive/$pkg/l
 
 cabal haddock $HADDOCK_OPTS --hyperlink-source $@
 
-cp -r extra/fonts dist/doc/html/snap/
 cp extra/logo.gif dist/doc/html/snap/haskell_icon.gif
 cp extra/hscolour.css dist/doc/html/snap/src/
