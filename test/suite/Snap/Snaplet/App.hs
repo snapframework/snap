dc3edc5d15e1b2ddca0ab8d40732cda84553c24d
Ozgun Ataman <ozataman@gmail.com>
2011-07-30 16:16:28 -0400

    Add session stuff to testsuite app

--- test/suite/Snap/Snaplet/App.hs
+++ test/suite/Snap/Snaplet/App.hs
@@ -13,6 +13,7 @@ import Control.Monad.Trans
 import Data.Maybe
 import Data.Record.Label
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import Data.Configurator
 import Snap.Types
 import Snap.Util.FileServe
@@ -23,11 +24,14 @@ import Text.Templating.Heist
 
 import Snap.Snaplet.FooSnaplet
 import Snap.Snaplet.BarSnaplet
+import Snap.Snaplet.Session
+import Snap.Snaplet.Session.Backends.CookieSession
 
 data App = App
     { _heist :: Snaplet (Heist App)
     , _foo :: Snaplet FooSnaplet
     , _bar :: Snaplet BarSnaplet
+    , _session :: Snaplet SessionManager
     }
 
 mkLabels [''App]
@@ -45,20 +49,50 @@ routeWithConfig = do
     val <- liftIO $ lookup cfg "topConfigField"
     writeText $ "routeWithConfig: " `T.append` fromJust val
 
+
+sessionDemo :: Handler App App ()
+sessionDemo = withSession session $ do
+  with session $ do
+    curVal <- getFromSession "foo"
+    case curVal of
+      Nothing -> do
+        setInSession "foo" "bar"
+      Just _ -> return ()
+  list <- with session $ (T.pack . show) `fmap` sessionToList
+  csrf <- with session $ (T.pack . show) `fmap` csrfToken
+  renderWithSplices "session"
+    [ ("session", liftHeist $ textSplice list)
+    , ("csrf", liftHeist $ textSplice csrf) ]
+
+sessionTest :: Handler App App ()
+sessionTest = withSession session $ do
+  q <- getParam "q"
+  val <- case q of
+    Just x -> do
+      let x' = T.decodeUtf8 x
+      with session $ setInSession "test" x'
+      return x'
+    Nothing -> maybe "" id `fmap` with session (getFromSession "test")
+  writeText val
+
 app :: SnapletInit App App
 app = makeSnaplet "app" "Test application" Nothing $ do
     hs <- nestSnaplet "heist" heist $ heistInit "templates"
     fs <- nestSnaplet "foo" foo fooInit
     bs <- nestSnaplet "" bar $ nameSnaplet "baz" $ barInit
+    sm <- nestSnaplet "session" session $ 
+          initCookieSessionManager "sitekey.txt" "_session" (Just (30 * 60))
     addSplices
         [("appsplice", liftHeist $ textSplice "contents of the app splice")]
     addRoutes [ ("/hello", writeText "hello world")
               , ("/routeWithSplice", routeWithSplice)
               , ("/routeWithConfig", routeWithConfig)
               , ("/public", serveDirectory "public")
+              , ("/sessionDemo", sessionDemo)
+              , ("/sessionTest", sessionTest)
               , ("/admin/reload", reloadSite)
               ]
     wrapHandlers (<|> heistServe)
-    return $ App hs fs bs
+    return $ App hs fs bs sm
 
 
