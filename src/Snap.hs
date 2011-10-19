e1f78d6a5755046e253e4c16868cebdef596740e
Mighty Byte <mightybyte@gmail.com>
2011-10-19 17:34:07 -0400

    Added Snap module with convenience imports.

---
+++ src/Snap.hs
@@ -0,0 +1,18 @@
+module Snap
+  ( module Control.Applicative
+  , module Control.Monad.State
+  , module Data.Lens.Common
+  , module Data.Lens.Template
+  , module Snap.Core
+  , module Snap.Http.Server
+  , module Snap.Snaplet
+  ) where
+
+import Control.Applicative
+import Control.Monad.State
+import Data.Lens.Common
+import Data.Lens.Template
+import Snap.Core
+import Snap.Http.Server
+import Snap.Snaplet
+
