48284e861600697f121d07bd5b672c7be0333b52
Gregory Collins <greg@gregorycollins.net>
2011-10-21 12:14:58 +0200

    Check in role-based access control code

---
+++ src/Data/RBAC/Internal/Role.hs
@@ -0,0 +1,80 @@
+module Data.RBAC.Internal.Role where
+
+import           Control.Monad.ST
+import           Data.Hashable
+import           Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as M
+import           Data.String
+import           Data.Text (Text)
+import qualified Data.Vector as V
+import qualified Data.Vector.Algorithms.Merge as VA
+
+
+------------------------------------------------------------------------------
+data RoleValue = RoleBool Bool
+               | RoleText Text
+               | RoleInt Int
+               | RoleDouble Double
+  deriving (Ord, Eq, Show)
+
+
+instance IsString RoleValue where
+    fromString = RoleText . fromString
+
+
+instance Hashable RoleValue where
+    hashWithSalt salt (RoleBool e)   = hashWithSalt salt e `combine` 7
+    hashWithSalt salt (RoleText t)   = hashWithSalt salt t `combine` 196613
+    hashWithSalt salt (RoleInt i)    = hashWithSalt salt i `combine` 12582917
+    hashWithSalt salt (RoleDouble d) = hashWithSalt salt d `combine` 1610612741
+
+
+------------------------------------------------------------------------------
+data Role = Role {
+      _roleName :: Text
+    , _roleData :: HashMap Text RoleValue
+    }
+  deriving (Eq, Show)
+
+
+instance IsString Role where
+    fromString s = Role (fromString s) M.empty
+
+
+------------------------------------------------------------------------------
+toSortedList :: (Ord k, Ord v) => HashMap k v -> [(k,v)]
+toSortedList m = runST $ do
+    v <- V.unsafeThaw $ V.fromList $ M.toList m
+    VA.sort v
+    v' <- V.unsafeFreeze v
+    return $ V.toList v'
+
+
+
+instance Hashable Role where
+    hashWithSalt salt (Role nm dat) =
+        h $ hashWithSalt salt nm
+      where
+        h s = hashWithSalt s $ toSortedList dat
+
+
+------------------------------------------------------------------------------
+data RoleValueMeta = RoleBoolMeta
+                   | RoleTextMeta
+                   | RoleEnumMeta [Text]
+                   | RoleIntMeta
+                   | RoleDoubleMeta
+
+
+data RoleDataDefinition = RoleDataDefinition {
+      _roleDataName        :: Text
+    , _roleValueMeta       :: RoleValueMeta
+    , _roleDataDescription :: Text
+    }
+
+
+data RoleMetadata = RoleMetadata {
+      _roleMetadataName :: Text
+    , _roleDescription  :: Text
+    , _roleDataDefs     :: [RoleDataDefinition]
+    }
---
+++ src/Data/RBAC/Role.hs
@@ -0,0 +1,24 @@
+module Data.RBAC.Role where
+
+import qualified Data.HashMap.Strict as M
+import           Data.RBAC.Internal.Types
+import           Data.Text (Text)
+
+
+------------------------------------------------------------------------------
+matches :: Role -> Role -> Bool
+matches (Role a1 d1) (Role a2 d2) =
+    a1 == a2 && dmatch (toSortedList d1) (toSortedList d2)
+  where
+    dmatch []         _      = True
+    dmatch _          []     = False
+    dmatch dds@(d:ds) (e:es) =
+        case compare d e of
+          LT -> False
+          EQ -> dmatch ds es
+          GT -> dmatch dds es
+
+
+------------------------------------------------------------------------------
+addRoleData :: Text -> RoleValue -> Role -> Role
+addRoleData k v (Role n d) = Role n $ M.insert k v d
