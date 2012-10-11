9074a84bed6a0d759204e031d0ea29d4dd58b2a6
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2012-10-11 19:25:06 +0200

    Used undefined as placeholder for (Snaplet b), one test passes

--- src/Snap/Snaplet/Test.hs
+++ src/Snap/Snaplet/Test.hs
@@ -7,5 +7,20 @@ module Snap.Snaplet.Test
   where
 
 
+import           Control.Monad.IO.Class
+import           Snap.Core
+import           Snap.Snaplet
+import           Snap.Snaplet.Internal.Types
+import           Snap.Test hiding (runHandler)
+import qualified Snap.Test as ST
+
+
+-- Then what you do is use the initializer (probably a SnapletInit) to construct the b.
+-- From there, it should be pretty simple to connect runBase and runHandler from Snap.Test
+
 ------------------------------------------------------------------------------
-runHandler = undefined
+runHandler :: MonadIO m =>
+             RequestBuilder m ()
+           -> Handler b b a
+           -> m Response
+runHandler rq h = ST.runHandler rq $ runPureBase h undefined
