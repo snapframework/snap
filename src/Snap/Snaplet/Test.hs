c25330fd237d05bfa422a0e6b62b6cc2538070e5
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2012-10-15 20:11:22 +0200

    Used Initializer in RunHandler. Still not completely satisfied, though

--- src/Snap/Snaplet/Test.hs
+++ src/Snap/Snaplet/Test.hs
@@ -6,23 +6,38 @@ module Snap.Snaplet.Test
   )
   where
 
-
+import           Control.Category
+import           Control.Concurrent.MVar
 import           Control.Monad.IO.Class
+import           Data.Text
 import           Snap.Core
 import           Snap.Snaplet
 import           Snap.Snaplet.Internal.Types
-import           Snap.Test hiding (runHandler)
+import           Snap.Test hiding (get, runHandler)
 import qualified Snap.Test as ST
+import           Snap.Snaplet.Internal.Initializer
 
 
 -- Then what you do is use the initializer (probably a SnapletInit) to construct the b.
 -- From there, it should be pretty simple to connect runBase and runHandler from Snap.Test
 
 ------------------------------------------------------------------------------
-runHandler :: MonadIO m =>
-             RequestBuilder m ()
+runHandler :: RequestBuilder IO ()
            -> Handler b b a
-           -> b
-           -> m Response
-runHandler rq h app = ST.runHandler rq $ runPureBase h app
+           -> SnapletInit b b
+           -> IO (Either Text Response)
+runHandler rq h (SnapletInit init) = do
+        app <- getSnaplet init
+        case app of
+            (Left e) -> return $ Left e
+            (Right (a,_)) -> do
+                res <- ST.runHandler rq $ runPureBase h a
+                return $ Right res
+
+
+getSnaplet :: Initializer b b (Snaplet b)
+           -> IO (Either Text (Snaplet b, InitializerState b))
+getSnaplet init = do
+        mvar <- newEmptyMVar
+        runInitializer mvar "" init
 
