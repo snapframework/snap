432e2fc7d8c23bf1185b21667ec40897d5d001a2
Mighty Byte <mightybyte@gmail.com>
2012-12-09 14:08:38 -0500

    Update heistServe docs

--- src/Snap/Snaplet/Heist.hs
+++ src/Snap/Snaplet/Heist.hs
@@ -199,10 +199,10 @@ cRenderAs ct t = withTop' heistLens (Unclassed.cRenderAs ct t)
 
 
 ------------------------------------------------------------------------------
--- | A handler that serves all the templates (similar to 'fileServe').  If the
--- template specified in the request path is not found, it returns 'empty'.
--- Also, this function does not serve any templates beginning with an
--- underscore.  This gives you a way to prevent some templates from being
+-- | A handler that serves all the templates (similar to 'serveDirectory').
+-- If the template specified in the request path is not found, it returns
+-- 'empty'.  Also, this function does not serve any templates beginning with
+-- an underscore.  This gives you a way to prevent some templates from being
 -- served.  For example, you might have a template that contains only the
 -- navbar of your pages, and you probably wouldn't want that template to be
 -- visible to the user as a standalone template.  So if you put it in a file
