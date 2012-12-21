fca09a76cddabcbdbde6db4b022399038cdd6e53
Taneb <nvd1234@gmail.com>
2012-12-21 23:47:47 +0000

    Replaced combine with hashWithSalt
    combine isn't exported by the newest version of Data.Hashable. Goes some way to fixing issue #60

--- src/Control/Access/RoleBased/Internal/Role.hs
+++ src/Control/Access/RoleBased/Internal/Role.hs
@@ -26,11 +26,11 @@ instance IsString RoleValue where
 
 ------------------------------------------------------------------------------
 instance Hashable RoleValue where
-    hashWithSalt salt (RoleBool e)   = hashWithSalt salt e `combine` 7
-    hashWithSalt salt (RoleText t)   = hashWithSalt salt t `combine` 196613
-    hashWithSalt salt (RoleInt i)    = hashWithSalt salt i `combine` 12582917
+    hashWithSalt salt (RoleBool e)   = hashWithSalt salt e `hashWithSalt` (7 :: Int)
+    hashWithSalt salt (RoleText t)   = hashWithSalt salt t `hashWithSalt` (196613 :: Int)
+    hashWithSalt salt (RoleInt i)    = hashWithSalt salt i `hashWithSalt` (12582917 :: Int)
     hashWithSalt salt (RoleDouble d) =
-        hashWithSalt salt d `combine` 1610612741
+        hashWithSalt salt d `hashWithSalt` (1610612741 :: Int)
 
 
 ------------------------------------------------------------------------------
