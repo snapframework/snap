1b1c13506b6e228b579b2546e0c7369bc80f0dc5
Greg Hale <ImAlsoGreg@gmail.com>
2014-06-16 23:51:04 -0400

    Add some tests

--- test/suite/Snap/Snaplet/Heist/App.hs
+++ test/suite/Snap/Snaplet/Heist/App.hs
@@ -14,20 +14,18 @@ module Snap.Snaplet.Heist.App
 
 
 ------------------------------------------------------------------------------
-import           Control.Applicative
-import           Control.Concurrent
 import           Control.Lens
-import           Control.Monad.IO.Class
 import           System.FilePath
 ------------------------------------------------------------------------------
+import           Data.Map.Syntax ((##))
+import qualified Heist.Interpreted                            as I
 import           Snap.Snaplet
 import           Snap.Snaplet.Auth
 import           Snap.Snaplet.Heist
 import           Snap.Snaplet.Session
 import           Snap.Snaplet.Auth.Backends.JsonFile
 import           Snap.Snaplet.Session.Backends.CookieSession
-
-import           Paths_snap (getDataDir)
+import qualified Text.XmlHtml                                 as XML
 
 ------------------------------------------------------------------------------
 data App = App
@@ -40,17 +38,15 @@ $(makeLenses ''App)
 instance HasHeist App where
     heistLens = subSnaplet heist
 
-
-------------------------------------------------------------------------------
-dataDir :: IO FilePath
-dataDir =  (</> "resources") <$> getDataDir
+aTestTemplate :: [XML.Node]
+aTestTemplate = [XML.TextNode "littleTemplateNode"]
 
 ------------------------------------------------------------------------------
 appInit :: SnapletInit App App
-appInit = makeSnaplet "foosnaplet" "Test application" (Just dataDir) $ do
+appInit = makeSnaplet "foosnaplet" "Test application" Nothing $ do
 
     h <- nestSnaplet "" heist $ heistInit "templates"
-    
+
     s <- nestSnaplet "sess" sess $
            initCookieSessionManager "site_key.txt" "sess" (Just 3600)
 
@@ -60,13 +56,11 @@ appInit = makeSnaplet "foosnaplet" "Test application" (Just dataDir) $ do
     addTemplates h "extraTemplates"
 
     sPath    <- getSnapletFilePath
-    let extraTemplatesPath = sPath </> "test" </> "heistTestApp" </> "templates" </> "evenMoreTemplates"
-    liftIO $ putStrLn . ("dataDir: " ++) . show =<< dataDir
-    liftIO $ print extraTemplatesPath
-    liftIO $ threadDelay 500000
+    let extraTemplatesPath = sPath </> "test" </> "evenMoreTemplates"
 
     addTemplatesAt h "evenMoreTemplates" extraTemplatesPath
-    
+    modifyHeistState (I.addTemplate "smallTemplate" aTestTemplate Nothing)
+
     return $ App s a h
 
 
--- test/suite/Snap/Snaplet/Heist/Tests.hs
+++ test/suite/Snap/Snaplet/Heist/Tests.hs
@@ -2,17 +2,24 @@
 
 module Snap.Snaplet.Heist.Tests where
 
+
+------------------------------------------------------------------------------
+import           Control.Applicative
 import           Control.Error
 import           Control.Exception
+import           Control.Monad.IO.Class               (liftIO)
 import qualified Data.ByteString                      as BS
 import           Data.List
+import qualified Data.Set                             as Set
 import qualified Data.Map                             as Map
+import qualified Data.Text                            as T
 import           Test.QuickCheck
 import           Test.HUnit
 import qualified Test.Framework as F
 import           Test.Framework.Providers.QuickCheck2
 import           Test.Framework.Providers.HUnit       (testCase)
 
+import           Data.Map.Syntax                      ((##))
 import qualified Heist                                as H
 import qualified Heist.Compiled                       as C
 import qualified Heist.Interpreted                    as I
@@ -25,35 +32,61 @@ import qualified Snap.Snaplet.Heist.Compiled          as C
 import qualified Snap.Snaplet.Heist.Interpreted       as I
 import           Snap.Snaplet.Heist.App
 import           SafeCWD
+import qualified Text.XmlHtml                         as XML
 
 heistTests :: F.Test
 heistTests = F.testGroup "Snap.Snaplet.Heist"
-             [addTemplatesOK
-             ,hasAllTemplates]
+             [testCase "Load templates" addTemplatesOK
+             ,testCase "Get Heist state" assertHasTemplates
+             ,testCase "Handler with heist state" accessibleHeistState
+             ,testCase "Render a small template" simpleRender]
 
 
 ------------------------------------------------------------------------------
-addTemplatesOK :: F.Test
-addTemplatesOK = testCase "Load templates" assertGoodLoadTpl
-  where
-    assertGoodLoadTpl :: Assertion
-    assertGoodLoadTpl = do
-      let hdl = with heist $ I.render "foopage"
-      res <- runHandler Nothing (ST.get "" Map.empty) hdl appInit
-      either (assertFailure . show) (ST.assertSuccess) res
+addTemplatesOK :: Assertion
+addTemplatesOK = do
+  let hdl = with heist $ I.render "foopage"
+  res <- runHandler Nothing (ST.get "" Map.empty) hdl appInit
+  either (assertFailure . show) (ST.assertSuccess) res
 
 
 ------------------------------------------------------------------------------
-hasAllTemplates :: F.Test
-hasAllTemplates = testCase "Get Heist state" assertHasTemplates
-  where
-    assertHasTemplates :: Assertion
-    assertHasTemplates = do
-      let hdl = with heist $  do
-            s <- getHeistState
-            t <- return $ H.templateNames s
-            return $ t
-      res <- evalHandler Nothing (ST.get "" Map.empty) hdl appInit
-      assertEqual "templateNames, minus foo, bar, and baz"
-        (Right []) res
+assertHasTemplates :: Assertion
+assertHasTemplates = do
+  let hdl = with heist $  do
+        s <- getHeistState
+        t <- return $ H.templateNames s
+        return $ Set.fromList (map head t)
+  res <- evalHandler Nothing (ST.get "" Map.empty) hdl appInit
+  assertBool "templateNames include foopage, barpage, bazpage" $ 
+    (Right (Set.fromList [])) ==
+    (Set.difference
+     (Set.fromList ["foopage","barpage","bazpage"])
+     <$> res)
 
+
+------------------------------------------------------------------------------
+accessibleHeistState :: Assertion
+accessibleHeistState = do
+  let hdl = with heist . withHeistState $
+        I.lookupSplice "thisSpliceDoesntExist"
+  res <- runHandler Nothing (ST.get "" Map.empty) hdl appInit
+  either (assertFailure . show) (ST.assertSuccess) res
+
+
+------------------------------------------------------------------------------
+simpleRender :: Assertion
+simpleRender = do
+--  let hdl = with heist $ do
+--        withHeistState $ \s -> do
+--          liftIO . putStrLn . show . H.spliceNames $ s
+--          gRender "smalTemplate"
+--      a = hdl :: Snap.Snaplet.Handler App App ()
+  let hdl = with heist $ gRender "barpage"
+  res <- runHandler Nothing (ST.get "" Map.empty) hdl appInit
+  either (assertFailure . show) ST.assertSuccess res
+  {-
+  assertEqual "Simple template rendering"
+    (Right [XML.TextNode "Content"])
+    res
+  -}
