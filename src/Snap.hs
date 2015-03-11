704f7bb5ce5949e744a3d2d205db5ebdfc031a8a
Doug Beardsley <mightybyte@gmail.com>
2015-03-11 15:17:21 -0400

    Remove re-exports, bump lens dependency

--- src/Snap.hs
+++ src/Snap.hs
@@ -7,17 +7,11 @@ see "Snap.Snaplet".  For the core web server API, see "Snap.Core".
 -}
 
 module Snap
-  ( module Control.Applicative
-  , module Control.Lens.Loupe
-  , module Control.Monad.State
-  , module Snap.Core
+  ( module Snap.Core
   , module Snap.Http.Server
   , module Snap.Snaplet
   ) where
 
-import Control.Applicative
-import Control.Lens.Loupe
-import Control.Monad.State
 import Snap.Core
 import Snap.Http.Server
 import Snap.Snaplet
